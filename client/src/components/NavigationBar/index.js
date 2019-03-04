8d17b9009674044879976c561c1c8bfe58914a45
Nikhil Vummadi <nrv26@njit.edu>
2019-03-03 19:46:23 -0500

    Setup the project

---
+++ client/src/components/NavigationBar/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
+
+const NavigationBar = () => (
+//     <Navbar bg="light" expand="lg">
+//     <Navbar.Brand href="#home">AlanytiQ</Navbar.Brand>
+//     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//     <Navbar.Collapse id="basic-navbar-nav">
+//       <Nav className="mr-auto">
+//         <Nav.Link href="#home">Home</Nav.Link>
+//         <Nav.Link href="#link"></Nav.Link>
+//         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+//           <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+//           <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+//         </NavDropdown>
+//       </Nav>
+//     </Navbar.Collapse>
+//   </Navbar> 
+    <p>Testing the navbar</p>
+);
+
+export default NavigationBar;
