c0066807bccd6a5066d6080a78eede2a6ad8cb3f
nori1026 <noritada@verizon.net>
2019-03-19 19:32:45 -0400

    modal centered, cancel button, X, login button toggle changed

--- client/src/components/Login/login.js
+++ client/src/components/Login/login.js
@@ -1,39 +1,51 @@
 //Login component
-import React, {Component} from 'react';
-import Modal from 'react-bootstrap/Modal';
-import './login.css';
-import ModalDialog from 'react-bootstrap/ModalDialog'
-import ModalHeader from 'react-bootstrap/ModalHeader'
-import ModalTitle from 'react-bootstrap/ModalTitle'
-import ModalBody from 'react-bootstrap/ModalBody'
-import ModalFooter from 'react-bootstrap/ModalFooter'
-
-
+import React, { Component } from "react";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import { Button } from "react-bootstrap";
+import "./login.css";
+import ModalDialog from "react-bootstrap/ModalDialog";
+import ModalHeader from "react-bootstrap/ModalHeader";
+import ModalTitle from "react-bootstrap/ModalTitle";
+import ModalBody from "react-bootstrap/ModalBody";
+import ModalFooter from "react-bootstrap/ModalFooter";
 
 //import { Modal, ModalDialog, modalHeader, ModalFooter, ModalTitle, ModalBody } from 'react-bootstrap/Modal'
 
-
-
 class Login extends Component {
-	render() {
-		return (
-			<div className="background">
-			<Modal.Dialog>
-				<Modal.Header closeButton>
-					<Modal.Title>Login</Modal.Title>
-				</Modal.Header>
-				<Modal.Body>
-					<input type="text" placeholder="Username"></input>
-					<input type="password" placeholder="Password"></input>
-				</Modal.Body>
-				<Modal.Footer>
-					<button>Cancel</button>
-					<button>Login</button>
-				</Modal.Footer>
-			</Modal.Dialog>
-			</div>
-		);
-	}
+  render() {
+    console.log(this.props);
+    return (
+      <div className="background">
+        <Modal.Dialog>
+          <Modal.Header closeButton onClick={this.props.loginBtn}>
+            <Modal.Title>Login</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+              </Form.Group>
+
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <button>Cancel</button>
+            <button>Login</button>
+          </Modal.Footer>
+        </Modal.Dialog>
+      </div>
+    );
+  }
 }
 
 export default Login;
--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -1,65 +1,53 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-<<<<<<< HEAD
-import { NavigationBar, Canvas, MyFiles } from '../../components';
-import Login from '../../components/Login/login';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { NavigationBar, Canvas, MyFiles } from "../../components";
+import Login from "../../components/Login/login";
 
 class App extends Component {
   state = {
     loginState: false
-  }
+  };
 
   loginBtn = () => {
-    if(this.state.loginState == true){
-      this.setState(
-        {
-          loginState: false
-        }
-      )
+    if (this.state.loginState == true) {
+      this.setState({
+        loginState: false
+      });
     } else {
       this.setState({
         loginState: true
-      })
+      });
     }
     console.log(this.state.loginState);
-  }
+  };
 
-=======
-import { NavigationBar, Canvas, MyFiles } from '../../components'
-
-class App extends Component {
->>>>>>> a8e30ac0ce9f3f1d5adc6fc543740dfb789b8849
-  uploadFile () {
-    console.log("File Uploading")
+  uploadFile() {
+    console.log("File Uploading");
   }
 
   render() {
-<<<<<<< HEAD
     let login;
-    if(this.state.loginState === true){
-      login = <div><Login/></div>
+    if (this.state.loginState === true) {
+      login = (
+        <div>
+          <Login loginBtn={this.loginBtn} />
+        </div>
+      );
     }
 
     return (
       <>
-        <NavigationBar uploadFile={this.uploadFile} loginBtn={this.loginBtn}/>
-=======
-    return (
-      <>
-        <NavigationBar uploadFile={this.uploadFile}/>
->>>>>>> a8e30ac0ce9f3f1d5adc6fc543740dfb789b8849
-        <div style={{display: 'flex', flexDirection: 'row'}}>
-          <div style={{flex: '1'}}>
-            <MyFiles/>
+        <NavigationBar uploadFile={this.uploadFile} loginBtn={this.loginBtn} />
+
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <div style={{ flex: "1" }}>
+            <MyFiles />
           </div>
-          <div style={{flex: '8', backgroundColor: '#a6a6a6'}}>
-            <Canvas/>
-          </div>
-<<<<<<< HEAD
           {login}
-=======
->>>>>>> a8e30ac0ce9f3f1d5adc6fc543740dfb789b8849
+          <div style={{ flex: "8", backgroundColor: "#a6a6a6" }}>
+            <Canvas />
+          </div>
         </div>
         {/* <div className="App">
           <header className="App-header">
