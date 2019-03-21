34a361a012e35b2eebcaebbefc412bf4c3d08d2c
ShortNeckGiraffe96 <nbartmon@gmail.com>
2019-03-21 14:42:19 -0400

    Changed the login box a little to look nicer

--- client/src/components/Login/login.js
+++ client/src/components/Login/login.js
@@ -9,6 +9,7 @@ import ModalHeader from "react-bootstrap/ModalHeader";
 import ModalTitle from "react-bootstrap/ModalTitle";
 import ModalBody from "react-bootstrap/ModalBody";
 import ModalFooter from "react-bootstrap/ModalFooter";
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
 
 //import { Modal, ModalDialog, modalHeader, ModalFooter, ModalTitle, ModalBody } from 'react-bootstrap/Modal'
 
@@ -25,7 +26,7 @@ class Login extends Component {
             <Modal.Title>Login</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <Form style={{ padding: "10rem" }}>
+            <Form>
               <Form.Group controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
                 <Form.Control type="email" placeholder="Enter email" />
@@ -35,19 +36,20 @@ class Login extends Component {
                 <Form.Label>Password</Form.Label>
                 <Form.Control type="password" placeholder="Password" />
               </Form.Group>
-
-              <Button variant="primary" type="submit">
+			  
+			  <Form.Group>
+              <Button variant="link">Forgot Password?</Button>
+              <Button variant="link">Register a new account</Button>
+			  </Form.Group>
+			  
+			  <Button variant="primary" type="submit" onClick={this.props.loginBtn}>
                 Submit
               </Button>
-
-              <Button variant="link">Forget Password</Button>
-              <Button variant="link">Register a new account</Button>
+			  <Button variant="secondary" onClick={this.props.loginBtn}>
+			    Cancel
+			  </Button>
             </Form>
           </Modal.Body>
-          <Modal.Footer>
-            <button onClick={this.props.loginBtn}>Cancel</button>
-            <button>Login</button>
-          </Modal.Footer>
         </Modal.Dialog>
       </div>
     );
