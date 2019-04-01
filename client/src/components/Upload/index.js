45afcba74d37196b57f319a7bdb817af304f4470
Noritada Okazaki <djokazaki@gmail.com>
2019-04-01 15:43:51 -0400

    merge

--- client/src/components/Upload/index.js
+++ client/src/components/Upload/index.js
@@ -3,6 +3,17 @@ import React, { Component } from "react";
 import { ButtonToolbar, Button, Modal, Form, ModalDialog, ModalHeader, ModalBody, ModalFooter } from 'react-bootstrap'
 
 //import { Modal, ModalDialog, modalHeader, ModalFooter, ModalTitle, ModalBody } from 'react-bootstrap/Modal'
+handleFileRead = (e) => {
+  const content = fileReader.result;
+  console.log(content);
+}
+
+handleFileChosen = (file) => {
+  fileReader = new fileReader();
+  fileReader.onloadend = handleFileRead;
+  fileReader.readAsText(file);
+}
+
 
 class Upload extends Component {
   render() {
@@ -18,7 +29,7 @@ class Upload extends Component {
           </Modal.Header>
           <Modal.Body>
             <Form>
-            <input type="file" id="fName">
+            <input type="file" id="fName" accept=".csv" onChange={e => handleFileChosen(e.target.files[0])}>
             </input>
             <br></br>
             <br></br>
