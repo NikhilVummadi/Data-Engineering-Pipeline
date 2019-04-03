0b2f5b061a35cc04f1bfc17dd557ee068b9e4983
Nikhil Vummadi <nrv26@njit.edu>
2019-04-03 15:53:12 -0400

    Spacing between private and public files

--- client/src/components/MyFiles/index.js
+++ client/src/components/MyFiles/index.js
@@ -7,21 +7,25 @@ class MyFiles extends Component {
     console.log(this.props);
 
     return (
-      <div>
-        <h4>Private</h4>
-        <Private
-          privateList={this.props.privateList}
-          fileSelection={this.props.fileSelection}
-          fillSidebar={this.props.fillSidebar}
-        />
-        <hr />
-        <h4>Public</h4>
-        <Public
-          publicList={this.props.publicList}
-          fileSelection={this.props.fileSelection}
-          fillBottombar={this.props.fillSidebar}
-        />
-      </div>
+        <>
+            <div style={{paddingLeft: 10, height: '30vh'}}>
+                <h4>Private</h4>
+                <Private
+                privateList={this.props.privateList}
+                fileSelection={this.props.fileSelection}
+                fillSidebar={this.props.fillSidebar}
+                />
+            </div>
+            <div style={{paddingLeft: 10, height: '30vh'}}>        
+                <hr />
+                <h4>Public</h4>
+                <Public
+                publicList={this.props.publicList}
+                fileSelection={this.props.fileSelection}
+                fillBottombar={this.props.fillSidebar}
+                />
+            </div>
+        </>
     );
   }
 }
