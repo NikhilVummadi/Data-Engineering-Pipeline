4a2654abe55aa2add331eba1c16f6f3e9786ee67
Noritada Okazaki <djokazaki@gmail.com>
2019-04-03 13:07:46 -0400

    all the other stuff

--- client/src/components/MyFiles/PrivateFiles/index.js
+++ client/src/components/MyFiles/PrivateFiles/index.js
@@ -1,21 +1,7 @@
 import React, { Component } from "react";
 import NavLink from "react-bootstrap/NavLink";
-<<<<<<< HEAD
-import 'react-checkbox-tree/lib/react-checkbox-tree.css';
-import CheckboxTree from 'react-checkbox-tree';
-
-const nodes = [{
-    value: this.props.id,
-    label: this.props.name,
-    children: [
-        { value: 'images', label: 'Images' },
-        { value: 'data', label: 'Data' },
-    ],
-}];
-=======
 import CheckboxTree from "react-checkbox-tree";
 import "react-checkbox-tree/lib/react-checkbox-tree.css";
->>>>>>> e059215751a20c01733d126b6cfa0fb7ee3b3576
 
 class PrivateFiles extends Component {
   constructor() {
@@ -56,16 +42,6 @@ class PrivateFiles extends Component {
     return (
       <div>
         <li style={this.getStyle()}>
-<<<<<<< HEAD
-        <CheckboxTree
-                nodes={nodes}
-                checked={this.state.checked}
-                expanded={this.state.expanded}
-                onCheck={checked => this.setState({ checked })}
-                onExpand={expanded => this.setState({ expanded })}
-                onClick={() => this.props.fileSelection("Private", this.props.name)}
-            />
-=======
           <CheckboxTree
             nodes={this.state.nodes}
             checked={this.state.checked}
@@ -75,7 +51,6 @@ class PrivateFiles extends Component {
             key={this.props.id}
             onClick={() => this.props.fileSelection("Private", this.props.name)}
           />
->>>>>>> e059215751a20c01733d126b6cfa0fb7ee3b3576
         </li>
       </div>
     );
